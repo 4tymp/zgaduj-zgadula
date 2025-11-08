151c4196f00b08b7dd6fc98d04ca00427a5f6fc3
tmp <tmp@192.168.1.10>
2025-11-08 10:01:04 +0100

    moved difficulty picker to a new function for that feature

--- zgaduj-zgadula.cpp
+++ zgaduj-zgadula.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
+#include <string>
 
-int main(){
-    //zbiorowisko zmiennych
+std::string poziomtrudnosci(){ //funkcja difficulty picker
     int trudnosc;
     std::string textrudnosc;
-
 
     std::cout << "wybierz poziom trudności:\n";
     std::cout << "(1)łatwy(1)   (2)średni(2)   (3)trudny(3)\n";
@@ -26,6 +25,29 @@ int main(){
     }else if(trudnosc == 3){
         textrudnosc = "trudny";
     }
+    return textrudnosc;
+}
 
+int main(){
+    //zbiorowisko zmiennych
+    std::string textrudnosc, rozpoczynacz;
+    std::string potwierdztrudnosc;
+    
+
+    //poczatek kodu,
+    poziomtrudnosci();
+
+    //potwierdzenie wyboru
     std::cout << "wybrałeś " << textrudnosc << " poziom trudności.\n";
+    std::cout << "zgadza się? y/n\n";
+    std::cin >> potwierdztrudnosc;
+    if (potwierdztrudnosc == "n" || potwierdztrudnosc == "N"){
+        poziomtrudnosci();
+    }else if (potwierdztrudnosc == "y" || potwierdztrudnosc == "Y"){
+        std::cout << "rozpoczynamy!";
+    }
+    
+    //DOKONCZ TO BO NIE DZIALA PETLA JAKAS CZY COS??
+    
+
 }
