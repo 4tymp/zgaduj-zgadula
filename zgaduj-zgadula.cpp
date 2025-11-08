a5806f3d019c0c9a3012705c9b85691165842582
tmp <tmp@192.168.1.10>
2025-11-08 10:21:02 +0100

    difficulty picker complete

--- zgaduj-zgadula.cpp
+++ zgaduj-zgadula.cpp
@@ -1,53 +1,49 @@
 #include <iostream>
 #include <string>
 
-std::string poziomtrudnosci(){ //funkcja difficulty picker
+int poziomtrudnosci(){ //funkcja difficulty picker
     int trudnosc;
     std::string textrudnosc;
+    std::string potwierdztrudnosc = "n";
 
-    std::cout << "wybierz poziom trudności:\n";
-    std::cout << "(1)łatwy(1)   (2)średni(2)   (3)trudny(3)\n";
+    while(potwierdztrudnosc != "t" && potwierdztrudnosc != "T" && potwierdztrudnosc != "y" && potwierdztrudnosc != "Y" ){
+        std::cout << "wybierz poziom trudności:\n";
+        std::cout << "(1)łatwy(1)   (2)średni(2)   (3)trudny(3)\n";
 
-    std::cin >> trudnosc;
-    
-    //sprawdza czy trudnosci sa w odpowiednim zakresie, wymusza wpisanie odpowiedniej liczby
-    while(trudnosc < 1 || trudnosc > 3){     
-        std::cout << "wybrano błędny poziom trudności.\n";
-        std::cout << "podaj poziom trudności jeszcze raz.\n";
         std::cin >> trudnosc;
-    }
+        
+        //sprawdza czy trudnosci sa w odpowiednim zakresie, wymusza wpisanie odpowiedniej liczby
+        while(trudnosc < 1 || trudnosc > 3){     
+            std::cout << "wybrano błędny poziom trudności.\n";
+            std::cout << "podaj poziom trudności jeszcze raz.\n";
+            std::cin >> trudnosc;
+        }
 
-    //ustawia odpowiedni string w zaleznosci od podanego poziomu trudnosci.
-    if(trudnosc == 1){
-        textrudnosc = "łatwy";
-    }else if(trudnosc == 2){
-        textrudnosc = "średni";
-    }else if(trudnosc == 3){
-        textrudnosc = "trudny";
+        //ustawia odpowiedni string w zaleznosci od podanego poziomu trudnosci.
+        if(trudnosc == 1){
+            textrudnosc = "łatwy";
+        }else if(trudnosc == 2){
+            textrudnosc = "średni";
+        }else if(trudnosc == 3){
+            textrudnosc = "trudny";
+        }
+
+        //potwierdzenie wyboru
+        std::cout << "wybrałeś " << textrudnosc << " poziom trudności.\n";
+        std::cout << "zgadza się? t/n\n";
+        std::cin >> potwierdztrudnosc;
+
     }
-    return textrudnosc;
+    return trudnosc;
 }
 
 int main(){
     //zbiorowisko zmiennych
-    std::string textrudnosc, rozpoczynacz;
-    std::string potwierdztrudnosc;
     
-
     //poczatek kodu,
-    poziomtrudnosci();
-
-    //potwierdzenie wyboru
-    std::cout << "wybrałeś " << textrudnosc << " poziom trudności.\n";
-    std::cout << "zgadza się? y/n\n";
-    std::cin >> potwierdztrudnosc;
-    if (potwierdztrudnosc == "n" || potwierdztrudnosc == "N"){
-        poziomtrudnosci();
-    }else if (potwierdztrudnosc == "y" || potwierdztrudnosc == "Y"){
-        std::cout << "rozpoczynamy!";
-    }
-    
-    //DOKONCZ TO BO NIE DZIALA PETLA JAKAS CZY COS??
+    int poztrud = poziomtrudnosci();
+    // dokoncz bro
+    std::cout << "poziom wybrany " << poztrud << "\n";
     
 
 }
